3c67f2a4bad0f1d2d60c6489b316b6dd2e181b89
iandavecustodio <99469899+iandavecustodio@users.noreply.github.com>
2024-12-09 22:36:01 +0800

    c

--- portfolio/r-resources/master-file.R
+++ portfolio/r-resources/master-file.R
@@ -1,2 +1,34 @@
 ##Set working directory
-setwd("C:/Users/Ian Dave B. Custodio/My Drive/OtherFiles/05 GitHub/iandavecustodio.github.io/portfolio/r-resources/quarto")
+setwd("C:/Users/Ian Dave B. Custodio/My Drive/OtherFiles/05 GitHub/iandavecustodio.github.io/portfolio/analyses/name1")
+
+###Quarto codes (copy codes starting from --- below):
+---
+title: "Untitled"
+format: html
+editor: visual
+---
+## Name: name
+### Institution: institution
+
+## Descriptive Statistics
+Summary Statistics of the variables:
+  
+
+
+
+## Running Code
+
+When you click the **Render** button a document will be generated that includes both content and the output of embedded code. You can embed code like this:
+  
+  ```{r}
+1 + 1
+```
+
+You can add options to executable code like this
+
+```{r}
+#| echo: false
+2 * 2
+```
+
+The `echo: false` option disables the printing of code (only output is displayed).
